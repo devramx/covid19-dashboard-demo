0ec798a2be80659eca3fa559193d72e387a1ee1f
devramx <dev.ramx@gmail.com>
2020-08-26 00:56:28 +0530

    Covid19 dashboard. Project commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Covid-19 Dashboard
+
+Simple dashboard to view worldwide cases/recovery/death statistics.
+
+[Demo](https://covid19-dash-v1.web.app/)
+
+## Screenshot
+![screenshot](./covid19-dashboard.png "screenshot")
---
+++ src/util.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { Circle, Popup } from "react-leaflet"
+import numeral from "numeral"
+
+const casesTypeColors = {
+    cases: {
+        hex: "#CC1034",
+        //   rgb: "rgb(204, 16, 52)",
+        //   half_op: "rgba(204, 16, 52, 0.5)",
+        multiplier: 800,
+    },
+    recovered: {
+        hex: "#7dd71d",
+        //   rgb: "rgb(125, 215, 29)",
+        //   half_op: "rgba(125, 215, 29, 0.5)",
+        multiplier: 1200,
+    },
+    deaths: {
+        hex: "#fb4443",
+        //   rgb: "rgb(251, 68, 67)",
+        //   half_op: "rgba(251, 68, 67, 0.5)",
+        multiplier: 2000,
+    },
+};
+
+export const sortData = (data) => {
+    const sortedData = [...data];
+    return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+}
+
+
+// Draw circles on the map with interactive tooltip
+export const showDataOnMap = (data, casesTypes = "cases") => (
+    data.map(country => (
+        <Circle
+            center={[country.countryInfo.lat, country.countryInfo.long]}
+            fillOpacity={0.4}
+            color={casesTypeColors[casesTypes].hex}
+            fillcolor={casesTypeColors[casesTypes].hex}
+            radius={
+                Math.sqrt(country[casesTypes]) * casesTypeColors[casesTypes].multiplier
+            }
+        >
+            <Popup >
+                <div className="info-container">
+                    <div
+                        className="info-flag"
+                        style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+                    ></div>
+                    <div className="info-name">{country.country}</div>
+                    <div className="info-confirmed">Cases: {numeral(country.cases).format(0, 0)}</div>
+                    <div className="info-recovered">Recovered: {numeral(country.recovered).format(0, 0)}</div>
+                    <div className="info-deaths">Deaths: {numeral(country.deaths).format(0, 0)}</div>
+                </div>
+            </Popup>
+        </Circle>
+    ))
+);
+
+
+export const prettyPrintStat = (stat) => 
+    stat ? `+${numeral(stat).format("0.0a")}` : stat; 
